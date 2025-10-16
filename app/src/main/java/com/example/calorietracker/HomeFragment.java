d0bc2d0afdca7ab0aa0404b6de2041eb4ff66ac4
sachinkathiresan <skathiresan@gatech.edu>
2025-10-16 17:07:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/calorietracker/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.calorietracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.calorietracker.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeFragmentBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = HomeFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(v ->
+                NavHostFragment.findNavController(HomeFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
